7bc113c570edb97b9ad6dbddfbce845fd27fc746
NiewiadomyYYXX <niewiadomyyyxx49@gmail.com>
2024-11-26 01:01:55 +0100

    Fixed Gui, Added DEC to BIN

--- Kalkulator Liczb/Kalkulator Liczb.cpp
+++ Kalkulator Liczb/Kalkulator Liczb.cpp
@@ -1,10 +1,32 @@
 ﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> 
 using namespace std;
 
 void welcome(int& choice, int& input);
 void printCalc(int& choice, int& input);
 
+vector<int> convert_binary(int input) {
+    if (input <= 0) { 
+        return { 0 }; 
+    }
+
+    vector<int> binary;
+
+    while (input > 0) {
+        binary.push_back(input % 2); 
+        input /= 2;                 
+    }
+
+    reverse(binary.begin(), binary.end());
+
+    return binary;
+}
+
 int main() {
+    cout << "\n\n";
+
     int choice;
     int input;
 
@@ -24,19 +46,48 @@ int main() {
 }
 
 void printCalc(int& choice, int& input) {
-    cout << "###################################### \n"
-        << "\n    Dziesietne: "
-        << "10" << "\n"
-        << "\n    Dwojkowe: "
-        << "2" << "\n"
-        << "\n    Osemkowy: "
-        << "8" << "\n"
-        << "\n    Szestnastkowy: "
-        << "16" << "\n"
-        << "\n    Czy jest palindromem: "
-        << "bool"
-        << "\n"
-        << "\n###################################### \n";
+    int ten = 0;
+    vector<int> binary;
+    int eight = 0;
+    string sixteen = "0";
+    bool isPalindrome = false;
+
+    switch (choice) {
+        case 1: {
+            ten = input;
+            binary = convert_binary(input);
+            break;
+        }
+        case 2: {
+            binary.push_back(input);
+            break;
+        }
+        case 3: {
+            eight = input;
+            break;
+        }
+        case 4: {
+            sixteen = input;
+            break;
+        }
+        default:
+            cout << "Blad" << endl;
+    }
+
+    cout << "###################################### \n";
+    cout << "\n    Dziesietny: ";
+    cout << ten;
+    cout << "\n\n    Dwojkowy: ";
+    for (int i = 0; i < binary.size(); ++i) {
+        cout << binary[i];
+    }
+    cout << "\n\n    Osemkowy: ";
+    cout << eight;
+    cout << "\n\n    Szestnastkowy: ";
+    cout << sixteen;
+    cout << "\n\n    Czy jest palindromem: " << (isPalindrome ? "Tak": "Nie");
+    cout << "\n" << "\n###################################### \n\n\n";
+
     system("pause");
     main();
 }
